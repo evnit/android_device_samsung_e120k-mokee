91cce4f211d2aed17d98ca7b0ff92f1ab29ce007
evnit <evnithn@gmail.com>
2014-04-27 09:16:32 +0700

    MoKee rom

---
+++ mokee.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, device/samsung/e120k/full_e120k.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120K TARGET_DEVICE=SHV-E120K BUILD_FINGERPRINT="samsung/SHV-E120K/SHV-E120K:4.4.2/KVT49L/KKMK1:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120K-user 4.4.2 KVT49L KKMK1 release-keys"
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_NAME := mokee_e120k
+PRODUCT_DEVICE := e120k
+
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo mokee_e120k-userdebug
+
